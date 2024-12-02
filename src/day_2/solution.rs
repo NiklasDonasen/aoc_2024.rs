3bd7644527a2112a7898a812dd0d291a1432cd19
Niklas Dønåsen <niklas.donasen@hafslund.no>
2024-12-02 16:30:09 +0100

    Get ready for day 3

---
+++ src/day_2/solution.rs
@@ -0,0 +1,78 @@
+use crate::get_reader;
+use std::env::current_dir;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::iter::Zip;
+use std::path::PathBuf;
+use std::slice::Iter;
+
+pub fn solve_day_2() -> anyhow::Result<()> {
+    // Reading the input file
+    let root: PathBuf = current_dir().expect("Not a valid directory.");
+    let reader: BufReader<File> = get_reader(&root.join("src").join("day_2").join("input.txt"));
+
+    // Structure the output
+    let mut safe_reps: i32 = 0;
+    let mut safe_reps_w_dampener: i32 = 0;
+
+    // Parse the input
+    for line in reader.lines() {
+        let line = line.expect("Not able to read line");
+
+        let line_parts: Vec<&str> = line.split(" ").collect();
+
+        let mut report: Vec<i32> = vec![];
+
+        for level in line_parts {
+            let level: i32 = level.parse().expect("Could not parse string to int.");
+            report.push(level);
+        }
+
+        // Q1 - Check if vector fullfils both conditions
+        if check_vector(&report) {
+            safe_reps += 1;
+            safe_reps_w_dampener += 1;
+        }
+        else {
+            // Q2 - apply the problem dampener
+            let num_elements = report.len();
+            for index in 0..num_elements {
+                let mut temp_report = report.clone();
+                temp_report.remove(index);
+
+                // Check until you get a true
+                if check_vector(&temp_report) {
+                    safe_reps_w_dampener += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("Answer to Q1 is {safe_reps}.");
+    println!("Answer to Q2 is {safe_reps_w_dampener}.");
+
+
+    Ok(())
+}
+
+fn check_vector(input: &Vec<i32>) -> bool {
+    // first condition: all in- or decreasing
+    let increasing: bool = input.windows(2).all(|w| w[0] <= w[1]);
+    let decreasing: bool = input.windows(2).all(|w| w[0] >= w[1]);
+
+    // Check the conditions
+    if increasing || decreasing {
+        // Now you can check second condition
+        // second condition: no big jumps
+        let jumps: bool = input.windows(2).all(|w| 1 <= (w[0] - w[1]).abs() && (w[0] - w[1]).abs() <= 3);
+        if jumps {
+            return  true
+        }
+    }
+    else {
+        return false;
+    }
+
+    false
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
 use std::env::current_dir;
+use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
-// mod day_1;
+// mod day_2;
 
 fn get_reader(path: &PathBuf) -> BufReader<File> {
     let file = File::open(path);
@@ -25,71 +25,14 @@ fn get_reader(path: &PathBuf) -> BufReader<File> {
 fn main() -> anyhow::Result<()> {
     // Reading the input file
     let root: PathBuf = current_dir().expect("Not a valid directory.");
-    let reader: BufReader<File> = get_reader(&root.join("src").join("day_2").join("input.txt"));
-
-    // Structure the output
-    let mut safe_reps: i32 = 0;
-    let mut safe_reps_w_dampener: i32 = 0;
+    let reader: BufReader<File> = get_reader(&root.join("src").join("day_3").join("input.txt"));
 
     // Parse the input
     for line in reader.lines() {
         let line = line.expect("Not able to read line");
 
-        let line_parts: Vec<&str> = line.split(" ").collect();
-
-        let mut report: Vec<i32> = vec![];
-
-        for level in line_parts {
-            let level: i32 = level.parse().expect("Could not parse string to int.");
-            report.push(level);
-        }
-
-        // Q1 - Check if vector fullfils both conditions
-        if check_vector(&report) {
-            safe_reps += 1;
-            safe_reps_w_dampener += 1;
-        }
-        else {
-            // Q2 - apply the problem dampener
-            let num_elements = report.len();
-            for index in 0..num_elements {
-                let mut temp_report = report.clone();
-                temp_report.remove(index);
-
-                // Check until you get a true
-                if check_vector(&temp_report) {
-                    safe_reps_w_dampener += 1;
-                    break;
-                }
-            }
-        }
+        println!("{line}");
     }
-
-    println!("Answer to Q1 is {safe_reps}.");
-    println!("Answer to Q2 is {safe_reps_w_dampener}.");
-
 
     Ok(())
-}
-
-fn check_vector(input: &Vec<i32>) -> bool {
-    // first condition: all in- or decreasing
-    let increasing: bool = input.windows(2).all(|w| w[0] <= w[1]);
-    let decreasing: bool = input.windows(2).all(|w| w[0] >= w[1]);
-
-    // Check the conditions
-    if increasing || decreasing {
-        println!("First condition satisfied");
-        // Now you can check second condition
-        // second condition: no big jumps
-        let jumps: bool = input.windows(2).all(|w| 1 <= (w[0] - w[1]).abs() && (w[0] - w[1]).abs() <= 3);
-        if jumps {
-            return  true
-        }
-    }
-    else {
-        return false;
-    }
-
-    false
 }

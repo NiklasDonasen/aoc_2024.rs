7951960dff1d87c1a1f61eb4d0b5ca83bc52996f
Niklas Dønåsen <niklas.donasen@hafslund.no>
2024-12-06 21:21:03 +0100

    Brute forcing part 2

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
-// use day_5::solution::solve_day_5;
-// mod day_5;
+// use day_6::solution::solve_day_6;
+// mod day_6;
 
 fn get_reader(path: &PathBuf) -> BufReader<File> {
     let file = File::open(path);
@@ -26,9 +26,8 @@ fn get_reader(path: &PathBuf) -> BufReader<File> {
 fn main() -> anyhow::Result<()> {
     // Reading the input file
     let root: PathBuf = current_dir().expect("Not a valid directory.");
-    let reader: BufReader<File> = get_reader(&root.join("src").join("day_5").join("input.txt"));
+    let reader: BufReader<File> = get_reader(&root.join("src").join("day_6").join("input.txt"));
 
-    // Parse the input
     for line in reader.lines() {
         let line = line.expect("Not able to read line");
     }
